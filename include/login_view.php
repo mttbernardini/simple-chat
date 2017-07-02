cce7d8ab597c3ca33080a062fe041b3271792f2e
Matteo Bernardini <mat_teobe@yahoo.it>
2017-07-02 17:06:30 +0200

    moar indentation fixes.

--- include/login_view.php
+++ include/login_view.php
@@ -1,28 +1,30 @@
 <form action="?aut=1" method="post" onmouseover="document.getElementById('input').focus();" style="text-align: center; margin: 0px 20%;">
 
-  <div>Per favore, immetti il tuo nick per iniziare a chattare.</div>
+	<div>Per favore, immetti il tuo nick per iniziare a chattare.</div>
 
-  <div>
-   <label>
-	 <span style="margin-right: 8px;">Nome:</span>
-	 <input class="text" type="text" name="name" id="input" maxlength="20" value="<?php echo $_COOKIE['username']; ?>" />
-	 <?php if($_GET['alreadylogged']) echo "<br /><span class=\"error\">Questo nickname è già stato utilizzato, oppure attendi $expireTime secondi e riprova</span>"; ?>
-	 <?php if($_GET['namerequired']) echo "<br /><span class=\"error\">È obbligatorio specificare un nickname</span>"; ?>
-   </label>
-  </div>
+	<div>
+		<label>
+			<span style="margin-right: 8px;">Nome:</span>
+			<input class="text" type="text" name="name" id="input" maxlength="20" value="<?php echo $_COOKIE['username']; ?>" />
+			<?php if($_GET['alreadylogged']) echo "<br /><span class=\"error\">Questo nickname è già stato utilizzato, oppure attendi $expireTime secondi e riprova</span>"; ?>
+			<?php if($_GET['namerequired']) echo "<br /><span class=\"error\">È obbligatorio specificare un nickname</span>"; ?>
+		</label>
+	</div>
 
-  <div style="padding-left: 60px; text-align: left;">
-   <label>
-	 <input type="checkbox" name="remember" <?php if($_COOKIE['remember']) echo 'checked="checked"'; ?> />
-	 <span>Memorizza il mio nickname</span>
-   </label><br />
-   <label>
-	 <input type="checkbox" name="autologin" <?php if($_COOKIE['autologin']) echo 'checked="checked"'; ?> />
-	 <span>Effettua il login automaticamente</span>
-   </label>
-  </div>
+	<div style="padding-left: 60px; text-align: left;">
+		<label>
+			<input type="checkbox" name="remember" <?php if($_COOKIE['remember']) echo 'checked="checked"'; ?> />
+			<span>Memorizza il mio nickname</span>
+		</label>
+		<br/>
+		<label>
+			<input type="checkbox" name="autologin" <?php if($_COOKIE['autologin']) echo 'checked="checked"'; ?> />
+			<span>Effettua il login automaticamente</span>
+		</label>
+	</div>
 
-  <div><button type="submit">Login</button></div>
-  <input type="hidden" name="aut" value="1" />
+	<div><button type="submit">Login</button></div>
+
+	<input type="hidden" name="aut" value="1" />
 
 </form>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (stripos($_SERVER['HTTP_USER_AGENT'], "msie") !== false) {
 	echo '<?xml version="1.0" encoding="UTF-8"?>'."\n".'<?xml-stylesheet type="text/xsl" href="./assets/iemode.xsl"?>'."\n";
 }
 else {
-	header("Content-type: application/xhtml+xml; charset=UTF-8"); 
+	header("Content-type: application/xhtml+xml; charset=UTF-8");
 	echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 }
 ?>
@@ -14,103 +14,97 @@ else {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=8"/><![endif]-->
-   <meta charset="UTF-8" />
-   <title><?= $title; ?></title>
-   <link href="./assets/css/main.css" rel="stylesheet" type="text/css" />
-   <link href="./assets/css/dialog_default.css" rel="stylesheet" type="text/css" />
-   <link href="./assets/css/dialog.css" rel="stylesheet" type="text/css" />
+	<meta charset="UTF-8" />
+	<title><?= $title; ?></title>
+	<link href="./assets/css/main.css" rel="stylesheet" type="text/css" />
+	<link href="./assets/css/dialog_default.css" rel="stylesheet" type="text/css" />
+	<link href="./assets/css/dialog.css" rel="stylesheet" type="text/css" />
 
-   <!--[if IE]>
-   <script type="text/javascript">
-      var html5 = ("article,aside,footer,header,hgroup,section,audio").split(',');
+	<!--[if IE]>
+	<script type="text/javascript">
+	var html5 = ("article,aside,footer,header,hgroup,section,audio").split(',');
 
-   for (var i = 0; i < html5.length; i++) {
-   var a=document.createElement(html5[i]);
-   }
-   </script>
+	for (var i = 0; i < html5.length; i++) {
+		var a=document.createElement(html5[i]);
+	}
+	</script>
 
-   <style type="text/css">
-   .delivering { filter: Alpha(Opacity=50); }
-   </style>
-   <![endif]-->
+	<style type="text/css">
+	.delivering { filter: Alpha(Opacity=50); }
+	</style>
+	<![endif]-->
 
-   <script type="text/javascript" src="./assets/js/get_variables.js"></script>
-   <script type="text/javascript" src="./assets/js/dialog.js"></script>
-   <script type="text/javascript">
-   <![CDATA[
-
-      var _GET = GETvars();
-      var dialogSettings = {
-      defTitle: "Messaggio",
-      defContent: "<i>Testo mancante</i>",
-      continueText: "Continua",
-      cancelText: "Annulla"
-      };
-      var timerAll, timerRenew;
-      var timeAll = <?php echo $refreshAll; ?>;
-      var timeRenew = <?php echo $renewSession; ?>;
-      var nickName = "<?php echo $nickname; ?>";
-
-   //]]>
-   </script>
-   <script type="text/javascript" src="./assets/js/main.js"></script>
-
+	<script type="text/javascript" src="./assets/js/get_variables.js"></script>
+	<script type="text/javascript" src="./assets/js/dialog.js"></script>
+	<script type="text/javascript">
+	<![CDATA[
+		var _GET = GETvars();
+		var dialogSettings = {
+			defTitle: "Messaggio",
+			defContent: "<i>Testo mancante</i>",
+			continueText: "Continua",
+			cancelText: "Annulla"
+		};
+		var timerAll, timerRenew;
+		var timeAll = <?php echo $refreshAll; ?>;
+		var timeRenew = <?php echo $renewSession; ?>;
+		var nickName = "<?php echo $nickname; ?>";
+	//]]>
+	</script>
+	<script type="text/javascript" src="./assets/js/main.js"></script>
 </head>
-
-
 
 <body onload="if (_GET['login']==1) { UpdateTimer(); doRenew(); }">
 
-  <audio id="new-msg" preload="auto">
-  <source type="audio/mpeg" src="./assets/audio/new_msg.mp3" />
-  <source type="audio/ogg" src="./assets/audio/new_msg.ogg" />
-  <embed type="audio/mpeg" src="./assets/audio/new_msg.mp3" enablejavascript="true" autoplay="false" />
-  </audio>
+	<audio id="new-msg" preload="auto">
+		<source type="audio/mpeg" src="./assets/audio/new_msg.mp3" />
+		<source type="audio/ogg" src="./assets/audio/new_msg.ogg" />
+		<embed type="audio/mpeg" src="./assets/audio/new_msg.mp3" enablejavascript="true" autoplay="false" />
+	</audio>
 
-  <audio id="send-msg" preload="auto">
-  <source type="audio/mpeg" src="./assets/audio/send_msg.mp3" />
-  <source type="audio/ogg" src="./assets/audio/send_msg.ogg" />
-  <embed type="audio/mpeg" src="./assets/audio/send_msg.mp3" enablejavascript="true" autoplay="false" />
-  </audio>
+	<audio id="send-msg" preload="auto">
+		<source type="audio/mpeg" src="./assets/audio/send_msg.mp3" />
+		<source type="audio/ogg" src="./assets/audio/send_msg.ogg" />
+		<embed type="audio/mpeg" src="./assets/audio/send_msg.mp3" enablejavascript="true" autoplay="false" />
+	</audio>
 
-  <audio id="new-user" preload="auto">
-  <source type="audio/mpeg" src="./assets/audio/new_user.mp3" />
-  <source type="audio/ogg" src="./assets/audio/new_user.ogg" />
-  <embed type="audio/mpeg" src="./assets/audio/new_user.mp3" enablejavascript="true" autoplay="false" />
-  </audio>
+	<audio id="new-user" preload="auto">
+		<source type="audio/mpeg" src="./assets/audio/new_user.mp3" />
+		<source type="audio/ogg" src="./assets/audio/new_user.ogg" />
+		<embed type="audio/mpeg" src="./assets/audio/new_user.mp3" enablejavascript="true" autoplay="false" />
+	</audio>
 
-  <audio id="exit-user" preload="auto">
-  <source type="audio/mpeg" src="./assets/audio/exit_user.mp3" />
-  <source type="audio/ogg" src="./assets/audio/exit_user.ogg" />
-  <embed type="audio/mpeg" src="./assets/audio/exit_user.mp3" enablejavascript="true" autoplay="false" />
-  </audio>
+	<audio id="exit-user" preload="auto">
+		<source type="audio/mpeg" src="./assets/audio/exit_user.mp3" />
+		<source type="audio/ogg" src="./assets/audio/exit_user.ogg" />
+		<embed type="audio/mpeg" src="./assets/audio/exit_user.mp3" enablejavascript="true" autoplay="false" />
+	</audio>
 
-  <div id="JSdisabled"><div>Devi attivare javascript per poter utilizzare la chat.</div></div>
-  <div id="noAJAX"><div>Il tuo browser purtroppo non supporta le richieste AJAX, pertanto la chat non è utilizzabile.<br />Scarica un browser più recente, come Google Chrome, Mozilla Firefox, o simili.</div></div>
+	<div id="JSdisabled"><div>Devi attivare javascript per poter utilizzare la chat.</div></div>
+	<div id="noAJAX"><div>Il tuo browser purtroppo non supporta le richieste AJAX, pertanto la chat non è utilizzabile.<br />Scarica un browser più recente, come Google Chrome, Mozilla Firefox, o simili.</div></div>
 
-    <div id="main">
-      <header style="height: 48px;">
-      <div id="icon"></div>
-      <h1><?= $title; ?></h1>
-      <div id="version"><?php echo '<a href="'.$versLink.'">Versione '.$version.'</a>'; ?></div>
-      </header>
+	<div id="main">
+		<header style="height: 48px;">
+			<div id="icon"></div>
+			<h1><?= $title; ?></h1>
+			<div id="version"><?php echo '<a href="'.$versLink.'">Versione '.$version.'</a>'; ?></div>
+		</header>
 
-      <section style="position: relative;">
-         <?php switchCases(); ?>
-      </section>
-    
-    </div>
+		<section style="position: relative;">
+			<?php switchCases(); ?>
+		</section>
+	</div>
 
-<script type="text/javascript">
-<![CDATA[
-document.getElementById("JSdisabled").style.display="none";
-if (_GET['login']==1) init();
-window.onunload=function() {
-parent.postMessage("Users:Offline", "*");
-parent.postMessage("NewMsg:-", "*");
-}
-//]]>
-</script>
+	<script type="text/javascript">
+	<![CDATA[
+		document.getElementById("JSdisabled").style.display="none";
+		if (_GET['login']==1) init();
+		window.onunload = function() {
+			parent.postMessage("Users:Offline", "*");
+			parent.postMessage("NewMsg:-", "*");
+		}
+	//]]>
+	</script>
 
 </body>
 </html>
